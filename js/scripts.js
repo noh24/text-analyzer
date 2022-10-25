aaa996fe1b8670faae76480bf42a7211071bf4d9
Brian Noh <noh24@ymail.com>
2022-10-25 13:22:03 -0400

    Add if statement to function to complete the 3rd test

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,13 @@
 function wordCounter(text) {
   let wordCount = 0;
   const textArray = text.split(" ");
+  console.log(textArray);
   textArray.forEach(function(word) {
-    wordCount++;
+    if (word.length === 0) {
+      return 0;
+    } else {
+      wordCount++;  
+    }
   });
   return wordCount;
 }
