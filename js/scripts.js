b30a4023afba8228029834d05a25589a642d971b
Brian Noh <noh24@ymail.com>
2022-10-25 14:11:26 -0400

    Add if condition to not count numbers as words

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,8 @@ function wordCounter(text) {
   textArray.forEach(function(word) {
     if (word.length === 0) {
       return 0;
+    } else if (parseFloat(word)) { 
+      wordCounter; //can be nothing
     } else {
       wordCount++;  
     }
